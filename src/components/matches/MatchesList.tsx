a566f7509f08a2dbac0ed68ba9e9fc27d5f812a7
eliashead <eliashead.costa@gmail.com>
2023-07-08 21:09:52 -0300

    feat: grid mobile matches

--- src/components/matches/MatchesList.tsx
+++ src/components/matches/MatchesList.tsx
@@ -38,93 +38,115 @@ export default async function MatchesList() {
   const matches = await getMatches()
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full">
-                <thead className="border-b">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 text-left text-sm font-medium text-gray-900"
-                    >
-                      #
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 text-left text-sm font-medium text-gray-900"
-                    >
-                      data
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 text-left text-sm font-medium text-gray-900"
-                    >
-                      Home
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 text-left text-sm font-medium text-gray-900"
-                    >
-                      -
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 text-left text-sm font-medium text-gray-900"
-                    >
-                      Visitante
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-4 text-left text-sm font-medium text-gray-900"
-                    >
-                      resultado
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {matches?.map((match: any) => {
-                    return (
-                      <tr className="border-b" key={match.match_id}>
-                        <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
-                          {match.match_id}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
-                          {formatDate(match.match_date)}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                          {match.home_team.team_name}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                          {match.home_goals}
-                          <span> x </span>
-                          {match.visitor_goals}
-                        </td>
-                        <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-                          {match.visitor_team.team_name}
-                        </td>
-                        <td
-                          style={{
-                            backgroundColor:
-                              match.result === 'red' ? 'red' : 'green',
-                            color: 'white',
-                            fontWeight: 'bold',
-                          }}
-                          className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900"
-                        >
-                          {match.result}
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+    <div className="w-[90%]">
+      <div className="hidden overflow-auto rounded-lg shadow md:inline-block">
+        <table className="min-w-full md:w-full">
+          <thead className="border-b">
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+              >
+                #
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+              >
+                data
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+              >
+                Home
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+              >
+                -
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+              >
+                Visitante
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+              >
+                resultado
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {matches?.map((match: any) => {
+              return (
+                <tr className="border-b" key={match.match_id}>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
+                    {match.match_id}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
+                    {formatDate(match.match_date)}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                    {match.home_team.team_name}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                    {match.home_goals}
+                    <span> x </span>
+                    {match.visitor_goals}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
+                    {match.visitor_team.team_name}
+                  </td>
+                  <td
+                    style={{
+                      backgroundColor: match.result === 'red' ? 'red' : 'green',
+                      color: 'white',
+                      fontWeight: 'bold',
+                    }}
+                    className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900"
+                  >
+                    {match.result}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
-    </>
+
+      <div className="grid grid-cols-1 gap-4 md:hidden">
+        {matches.map((match: any) => {
+          return (
+            <div
+              key={match.match_id}
+              className="space-y-3 rounded-lg bg-green-200 p-4 shadow-lg"
+            >
+              <div className="flex items-center space-x-2 text-sm">
+                <span>Data:</span>
+                <div>{formatDate(match.match_date)}</div>
+              </div>
+              <div className="flex items-center space-x-2 text-sm">
+                <div>{match.home_team.team_name}</div>
+                <div>
+                  {match.home_goals}
+                  <span> x </span>
+                  {match.visitor_goals}
+                </div>
+                <div>{match.visitor_team.team_name}</div>
+              </div>
+              <div className="flex items-center space-x-2 text-sm">
+                <span>Reasultado:</span>
+                <div>{match.result}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
