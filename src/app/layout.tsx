f6c652e05219c9cfdb1837a697c5457eee8ba5eb
eliashead <eliashead.costa@gmail.com>
2023-07-24 15:54:13 -0300

    fix: bg html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en bg-white">
       <body className={inter.className}>
         <Navbar />
         {children}
