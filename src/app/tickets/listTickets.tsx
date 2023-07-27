5cb3c0b11610d9b3319927dde0ad63cbb7c0cc7e
ELIASHEAD <eliashead.costa@gmail.com>
2023-07-27 17:32:58 -0300

    feat: pagination tickets

--- src/app/tickets/listTickets.tsx
+++ src/app/tickets/listTickets.tsx
@@ -3,6 +3,8 @@ import { Tickets } from '@prisma/client'
 import { DeleteTicket } from './deleteTicket'
 import { UpdateTicket } from './updateTicket'
 import { AddTicket } from './addTicket'
+import { useState } from 'react'
+import Pagination from '@/components/pagination/pagination'
 
 interface Match extends Tickets {
   Matches?: object[]
@@ -10,12 +12,35 @@ interface Match extends Tickets {
 }
 
 export const ListTickets = ({ tickets }: { tickets: Match[] }) => {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const itemsPerPage = 10
+
+  const totalPages = Math.ceil(tickets.length / itemsPerPage)
+
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+
+  const paginatedItems = tickets.slice(startIndex, endIndex)
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
   return (
     <div className="grid w-full grid-cols-1 gap-1">
       <div>
         <AddTicket />
       </div>
-      {tickets.map((ticket) => {
+      {paginatedItems.map((ticket) => {
         return (
           <div
             className="grid grid-cols-5 items-center gap-4 rounded px-4 py-4 shadow-lg sm:m-auto sm:px-8"
@@ -45,6 +70,16 @@ export const ListTickets = ({ tickets }: { tickets: Match[] }) => {
           </div>
         )
       })}
+
+      {totalPages > 1 && (
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          goToNextPage={goToNextPage}
+          goToPreviousPage={goToPreviousPage}
+        />
+      )}
     </div>
   )
 }
