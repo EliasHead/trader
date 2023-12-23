76c363c296cc2320fffc7a35c7051da31fa6fa4a
ELIASHEAD <eliashead.costa@gmail.com>
2023-12-23 11:17:26 -0300

    get competitions per number of greens

--- src/app/competitions/page.tsx
+++ src/app/competitions/page.tsx
@@ -1,21 +1,20 @@
+import { Competition } from '@prisma/client';
 import SearchCompetition from './SearchCompetition'
 import { db as prisma } from '@/lib/db'
 
 const getCompetitions = async () => {
-  const res = await prisma.competition.findMany({
-    select: {
-      competition_id: true,
-      competition_name: true,
-      season_name: true,
-      createdAt: true,
-    },
-  })
-  return res
+  const result: Competition[] = await prisma.$queryRaw`
+    SELECT c.*, COUNT(m.match_id) as green_matches_count
+    FROM competitions c
+    LEFT JOIN matches m ON c.competition_id = m.competition_id AND m.result = 'green'
+    GROUP BY c.competition_id
+    ORDER BY green_matches_count DESC`;
+  
+  return result
 }
 
 const Competitions = async () => {
   const competitions = await getCompetitions()
-
   return (
     <main className="mt-16 flex h-screen flex-col items-center justify-start gap-4">
       <h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import { FilterCompetitions } from '@/components/filterCompetitions'
 import { FilterEstrategies } from '@/components/filterEstrategies'
 import { db as prisma } from '@/lib/db'
+import { Competition } from '@prisma/client';
+
+const getCompetitions = async () => {
+  const result: Competition[] = await prisma.$queryRaw`
+    SELECT c.*, COUNT(m.match_id) as green_matches_count
+    FROM competitions c
+    LEFT JOIN matches m ON c.competition_id = m.competition_id AND m.result = 'green'
+    GROUP BY c.competition_id
+    ORDER BY green_matches_count DESC`;
+  
+  return result
+}
 
 export default async function Home() {
   const matches = await prisma.matches.findMany()
-  const competitions = await prisma.competition.findMany()
-
+  const competitions = await getCompetitions()
+  
   return (
     <main className="min-h-screen bg-background">
       <FilterCompetitions competitions={competitions} matches={matches} />
