b429949bca924de5bda27b0b34dba4ee8107b865
eliashead <eliashead.costa@gmail.com>
2024-01-18 15:47:31 -0300

    data

--- src/utils/rounds.ts
+++ src/utils/rounds.ts
@@ -42,4 +42,5 @@ export const dataRounds = [
   { round_id: 41, round_name: 'SEMI-FINALS' },
   { round_id: 42, round_name: 'FINAL' },
   { round_id: 43, round_name: '1/32-FINALS' },
+  { round_id: 44, round_name: 'OITAVAS DE FINAL' },
 ]
