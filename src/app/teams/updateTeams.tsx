8b9e31a520a6cafcf6a764ecb7590376c0e79487
ELIASHEAD <eliashead.costa@gmail.com>
2024-02-25 16:57:28 -0300

    update team

--- src/app/teams/updateTeams.tsx
+++ src/app/teams/updateTeams.tsx
@@ -1,7 +1,5 @@
 'use client'
-
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
 import axios from 'axios'
 import { Button } from '@/components/ui/button'
 import {
@@ -29,8 +27,10 @@ import {
 import { Loader2 } from 'lucide-react'
 
 import { TeamType } from './types'
+import { useQueryClientInstance } from '@/context/query-client-context-client'
+import { useMutation } from '@tanstack/react-query'
 
-const FormSchema = z.object({
+const FormUpdateTeamSchema = z.object({
   team_name: z.string({
     required_error: 'Please select a home team.',
   }),
@@ -39,30 +39,38 @@ const FormSchema = z.object({
   }),
 })
 
+type UpdateTeamSchema = z.infer<typeof FormUpdateTeamSchema>
+
 export const UpdateTeams = ({ team }: { team: TeamType }) => {
-  const [isLoading, setIsLoading] = useState(false)
+  const { queryClient } = useQueryClientInstance()
+  // const [isLoading, setIsLoading] = useState(false)
   const [open, setOpen] = useState(false)
 
-  const router = useRouter()
+  // const router = useRouter()
 
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+  const form = useForm<UpdateTeamSchema>({
+    resolver: zodResolver(FormUpdateTeamSchema),
     defaultValues: {
       team_name: team.team_name,
       team_country: team.team_country,
     },
   })
 
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
-    setIsLoading(true)
-    await axios.patch(`/api/teams/${team.team_id}`, {
-      team_name: data.team_name,
-      team_country: data.team_country,
-    })
+  const { mutate: updateTeam, isPending } = useMutation({
+    mutationFn: async (data: UpdateTeamSchema) =>
+      await axios.patch(`/api/teams/${team.team_id}`, {
+        team_name: data.team_name,
+        team_country: data.team_country,
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['teams'] })
+    },
+  })
 
-    setIsLoading(false)
+  async function onSubmit(data: UpdateTeamSchema) {
+    updateTeam(data)
     setOpen(!open)
-    router.refresh()
+    form.reset()
   }
 
   return (
@@ -124,7 +132,7 @@ export const UpdateTeams = ({ team }: { team: TeamType }) => {
                     Close
                   </Button>
                 </DialogClose>
-                {!isLoading ? (
+                {!isPending ? (
                   <Button type="submit">Save</Button>
                 ) : (
                   <Button type="button" disabled>
